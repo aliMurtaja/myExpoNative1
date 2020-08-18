17000bbb5231ab403a27b018ada46ffbfac4f435
murtaja ali <murtajaali10@gmail.com>
2020-08-18 07:18:48 +0530

    this is my first commit  for nativeVir1

---
+++ client/Components/TodoApp2/AllConfigs/api.js
@@ -0,0 +1,22 @@
+import { SERVER_URL } from './address';
+
+const tweetUrl = SERVER_URL;
+
+const config = (url, method, body) =>
+// console.log(SERVER_URL)
+	fetch(url, {
+		method,
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	}).then(response => response.json());
+
+// export const fetchTweets = () => fetch(tweetUrl).then(res => res.json());
+
+export const createTweets = ({ user, tweet }) => config(tweetUrl, 'POST', { user, tweet });
+
+// export const deleteTweet = ({ id }) => config(tweetUrl, 'DELETE', { id });
+
+// // export const logIn = ({ email, password }) => config(userUrl, 'POST', { email, password });
---
+++ client/Components/TodoApp2/Main/Pages/showItemes.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Image, Button, AsyncStorage } from 'react-native';
+
+export default (props) =>{
+    return (
+        <View>
+            <Text>show items</Text>
+        </View>
+    )
+    
+}
